dc9eb5c4d0ef1469e501ceffe8ec9ed9013a2da4
Evgeniy Moiseenko <s1lverhron@gmail.com>
2018-06-09 16:41:11 +0300

    Code Style

--- KCaptcha.php
+++ KCaptcha.php
@@ -26,20 +26,20 @@ class KCaptcha
 	 * Do not change without changing font files!
 	 * @var string
 	 */
-	private $alphabet = '0123456789abcdefghijklmnopqrstuvwxyz';
+	private $_alphabet = '0123456789abcdefghijklmnopqrstuvwxyz';
 
 	/**
 	 * Symbols used to draw CAPTCHA.
 	 * Alphabet without similar symbols (o=0, 1=l, i=j, t=f)
 	 * @var string
 	 */
-	private $allowed_symbols = '23456789abcdegikpqsvxyz';
+	private $_allowedSymbols = '23456789abcdegikpqsvxyz';
 
 	/**
 	 * Folder with fonts
 	 * @var string
 	 */
-	private $fontsDir = 'fonts';
+	private $_fontsDir = 'fonts';
 
 	/**
 	 * Generates keystring and image
@@ -48,31 +48,31 @@ class KCaptcha
 	public function KCAPTCHA() {
 		require __DIR__.'/kcaptcha_config.php';
 		$fonts = array();
-		$fontsdir_absolute = __DIR__.DIRECTORY_SEPARATOR.$this->fontsDir;
+		$fontsdir_absolute = __DIR__.DIRECTORY_SEPARATOR.$this->_fontsDir;
 		if ($handle = opendir($fontsdir_absolute)) {
 			while (false !== ($file = readdir($handle))) {
 				if (preg_match('/\.png$/i', $file)) {
-					$fonts[]=$fontsdir_absolute.'/'.$file;
+					$fonts[] = $fontsdir_absolute.'/'.$file;
 				}
 			}
 			closedir($handle);
 		}
 	
-		$alphabet_length = strlen($this->alphabet);
+		$alphabet_length = strlen($this->_alphabet);
 		
 		do {
 			// generating random keystring
 			while (true) {
 				$this->keystring = '';
 				for ($i = 0; $i < $length; $i++) {
-					$this->keystring .= $this->allowed_symbols{mt_rand(0,strlen($this->allowed_symbols)-1)};
+					$this->keystring .= $this->_allowedSymbols{mt_rand(0, strlen($this->_allowedSymbols) - 1)};
 				}
 				if (!preg_match('/cp|cb|ck|c6|c9|rn|rm|mm|co|do|cl|db|qp|qb|dp|ww/', $this->keystring)) {
 					break;
 				}
 			}
 		
-			$font_file = $fonts[mt_rand(0, count($fonts)-1)];
+			$font_file = $fonts[mt_rand(0, count($fonts) - 1)];
 			$font = imagecreatefrompng($font_file);
 			imagealphablending($font, true);
 
@@ -88,13 +88,13 @@ class KCaptcha
 				$transparent = (imagecolorat($font, $i, 0) >> 24) == 127;
 
 				if (!$reading_symbol && !$transparent) {
-					$font_metrics[$this->alphabet{$symbol}] = array('start'=>$i);
+					$font_metrics[$this->_alphabet{$symbol}] = array('start' => $i);
 					$reading_symbol = true;
 					continue;
 				}
 
 				if ($reading_symbol && $transparent) {
-					$font_metrics[$this->alphabet{$symbol}]['end'] = $i;
+					$font_metrics[$this->_alphabet{$symbol}]['end'] = $i;
 					$reading_symbol = false;
 					$symbol++;
 					continue;
@@ -106,7 +106,7 @@ class KCaptcha
 			$white = imagecolorallocate($img, 255, 255, 255);
 			$black = imagecolorallocate($img, 0, 0, 0);
 
-			imagefilledrectangle($img, 0, 0, $width-1, $height-1, $white);
+			imagefilledrectangle($img, 0, 0, $width - 1, $height - 1, $white);
 
 			// draw text
 			$x = 1;
@@ -115,18 +115,18 @@ class KCaptcha
 				$odd =- 1;
 			}
 			for ($i = 0; $i < $length; $i++) {
-				$m=$font_metrics[$this->keystring{$i}];
+				$m = $font_metrics[$this->keystring{$i}];
 
-				$y = (($i%2)*$fluctuation_amplitude - $fluctuation_amplitude/2)*$odd
-					+ mt_rand(-round($fluctuation_amplitude/3), round($fluctuation_amplitude/3))
-					+ ($height-$fontfile_height)/2;
+				$y = (($i % 2) * $fluctuation_amplitude - $fluctuation_amplitude/2) * $odd
+					+ mt_rand(-round($fluctuation_amplitude / 3), round($fluctuation_amplitude / 3))
+					+ ($height - $fontfile_height) / 2;
 
 				if ($no_spaces) {
 					$shift = 0;
 					if ($i > 0) {
 						$shift = 10000;
 						for ($sy = 3; $sy < $fontfile_height-10; $sy += 1) {
-							for ($sx = $m['start']-1; $sx < $m['end']; $sx += 1) {
+							for ($sx = $m['start'] - 1; $sx < $m['end']; $sx += 1) {
 								$rgb = imagecolorat($font, $sx, $sy);
 								$opacity = $rgb>>24;
 								if ($opacity < 127) {
@@ -173,7 +173,7 @@ class KCaptcha
 		$center = $x / 2;
 
 		// credits. To remove, see configuration file
-		$img2 = imagecreatetruecolor($width, $height + ($show_credits?12:0));
+		$img2 = imagecreatetruecolor($width, $height + ($show_credits ? 12 : 0));
 		$foreground = imagecolorallocate($img2, $foreground_color[0], $foreground_color[1], $foreground_color[2]);
 		$background = imagecolorallocate($img2, $background_color[0], $background_color[1], $background_color[2]);
 		imagefilledrectangle($img2, 0, 0, $width - 1, $height - 1, $background);
@@ -182,18 +182,18 @@ class KCaptcha
 		imagestring($img2, 2, $width / 2 - imagefontwidth(2) * strlen($credits) / 2, $height - 2, $credits, $background);
 
 		// periods
-		$rand1 = mt_rand(750000,1200000)/10000000;
-		$rand2 = mt_rand(750000,1200000)/10000000;
-		$rand3 = mt_rand(750000,1200000)/10000000;
-		$rand4 = mt_rand(750000,1200000)/10000000;
+		$rand1 = mt_rand(750000, 1200000) / 10000000;
+		$rand2 = mt_rand(750000, 1200000) / 10000000;
+		$rand3 = mt_rand(750000, 1200000) / 10000000;
+		$rand4 = mt_rand(750000, 1200000) / 10000000;
 		// phases
-		$rand5 = mt_rand(0,31415926)/10000000;
-		$rand6 = mt_rand(0,31415926)/10000000;
-		$rand7 = mt_rand(0,31415926)/10000000;
-		$rand8 = mt_rand(0,31415926)/10000000;
+		$rand5 = mt_rand(0, 31415926) / 10000000;
+		$rand6 = mt_rand(0, 31415926) / 10000000;
+		$rand7 = mt_rand(0, 31415926) / 10000000;
+		$rand8 = mt_rand(0, 31415926) / 10000000;
 		// amplitudes
-		$rand9 = mt_rand(330,420)/110;
-		$rand10 = mt_rand(330,450)/100;
+		$rand9 = mt_rand(330, 420) / 110;
+		$rand10 = mt_rand(330, 450) / 100;
 
 		//wave distortion
 		for ($x = 0; $x < $width; $x++) {
