5c3287046504ba1eca9faf62246a6a5b61e018b1
Evgeniy Moiseenko <s1lverhron@gmail.com>
2018-06-09 14:55:49 +0300

    Code style

--- KCaptcha.php
+++ KCaptcha.php
@@ -29,8 +29,7 @@ class KCaptcha
 	 * Generates keystring and image
 	 * KCaptcha constructor.
 	 */
-	public function KCAPTCHA()
-    {
+	public function KCAPTCHA() {
 		require(dirname(__FILE__) . '/kcaptcha_config.php');
 		$fonts = array();
 		$fontsdir_absolute = dirname(__FILE__).'/'.$fontsdir;
@@ -40,8 +39,8 @@ class KCaptcha
 					$fonts[]=$fontsdir_absolute.'/'.$file;
 				}
 			}
-		    closedir($handle);
-		}	
+			closedir($handle);
+		}
 	
 		$alphabet_length = strlen($this->alphabet);
 		
@@ -84,7 +83,7 @@ class KCaptcha
 				}
 			}
 
-			$img=imagecreatetruecolor($width, $height);
+			$img = imagecreatetruecolor($width, $height);
 			imagealphablending($img, true);
 			$white = imagecolorallocate($img, 255, 255, 255);
 			$black = imagecolorallocate($img, 0, 0, 0);
@@ -108,14 +107,14 @@ class KCaptcha
 						$shift = 10000;
 						for ($sy = 3; $sy < $fontfile_height-10; $sy += 1) {
 							for ($sx = $m['start']-1; $sx < $m['end']; $sx += 1) {
-				        		$rgb = imagecolorat($font, $sx, $sy);
-				        		$opacity = $rgb>>24;
+								$rgb = imagecolorat($font, $sx, $sy);
+								$opacity = $rgb>>24;
 								if ($opacity < 127) {
 									$left = $sx - $m['start'] + $x;
 									$py = $sy + $y;
 									if ($py > $height) break;
 									for ($px = min($left, $width-1); $px > $left - 200 && $px >= 0; $px -= 1) {
-						        		$color = imagecolorat($img, $px, $py) & 0xff;
+										$color = imagecolorat($img, $px, $py) & 0xff;
 										if ($color + $opacity < 170) { // 170 - threshold
 											if ($shift > $left - $px) {
 												$shift = $left - $px;
@@ -134,7 +133,7 @@ class KCaptcha
 				} else {
 					$shift = 1;
 				}
-				imagecopy($img, $font, $x-$shift, $y, $m['start'], 1, $m['end']-$m['start'], $fontfile_height);
+				imagecopy($img, $font, $x - $shift, $y, $m['start'], 1, $m['end'] - $m['start'], $fontfile_height);
 				$x += $m['end'] - $m['start'] - $shift;
 			}
 		} while ($x >= $width - 10); // while not fit in canvas
@@ -142,23 +141,23 @@ class KCaptcha
 		//noise
 		$white = imagecolorallocate($font, 255, 255, 255);
 		$black = imagecolorallocate($font, 0, 0, 0);
-		for ($i = 0; $i < (($height-30)*$x)*$white_noise_density; $i++) {
-			imagesetpixel($img, mt_rand(0, $x-1), mt_rand(10, $height-15), $white);
+		for ($i = 0; $i < (($height - 30) * $x) * $white_noise_density; $i++) {
+			imagesetpixel($img, mt_rand(0, $x - 1), mt_rand(10, $height - 15), $white);
 		}
-		for ($i = 0; $i < (($height-30)*$x)*$black_noise_density; $i++) {
-			imagesetpixel($img, mt_rand(0, $x-1), mt_rand(10, $height-15), $black);
+		for ($i = 0; $i < (($height - 30) * $x) * $black_noise_density; $i++) {
+			imagesetpixel($img, mt_rand(0, $x - 1), mt_rand(10, $height - 15), $black);
 		}
 
-		$center = $x/2;
+		$center = $x / 2;
 
 		// credits. To remove, see configuration file
-		$img2 = imagecreatetruecolor($width, $height+($show_credits?12:0));
+		$img2 = imagecreatetruecolor($width, $height + ($show_credits?12:0));
 		$foreground = imagecolorallocate($img2, $foreground_color[0], $foreground_color[1], $foreground_color[2]);
 		$background = imagecolorallocate($img2, $background_color[0], $background_color[1], $background_color[2]);
-		imagefilledrectangle($img2, 0, 0, $width-1, $height-1, $background);		
-		imagefilledrectangle($img2, 0, $height, $width-1, $height+12, $foreground);
-		$credits = empty($credits)?$_SERVER['HTTP_HOST']:$credits;
-		imagestring($img2, 2, $width/2-imagefontwidth(2)*strlen($credits)/2, $height-2, $credits, $background);
+		imagefilledrectangle($img2, 0, 0, $width - 1, $height - 1, $background);
+		imagefilledrectangle($img2, 0, $height, $width - 1, $height + 12, $foreground);
+		$credits = empty($credits) ? $_SERVER['HTTP_HOST'] : $credits;
+		imagestring($img2, 2, $width / 2 - imagefontwidth(2) * strlen($credits) / 2, $height - 2, $credits, $background);
 
 		// periods
 		$rand1 = mt_rand(750000,1200000)/10000000;
@@ -175,19 +174,18 @@ class KCaptcha
 		$rand10 = mt_rand(330,450)/100;
 
 		//wave distortion
-
 		for ($x = 0; $x < $width; $x++) {
 			for ($y = 0; $y < $height; $y++) {
-				$sx = $x+(sin($x*$rand1+$rand5)+sin($y*$rand3+$rand6))*$rand9-$width/2+$center+1;
-				$sy = $y+(sin($x*$rand2+$rand7)+sin($y*$rand4+$rand8))*$rand10;
+				$sx = $x + (sin($x*$rand1 + $rand5) + sin($y * $rand3 + $rand6)) * $rand9 - $width / 2 + $center + 1;
+				$sy = $y + (sin($x*$rand2 + $rand7) + sin($y * $rand4 + $rand8)) * $rand10;
 
-				if ($sx < 0 || $sy < 0 || $sx >= $width-1 || $sy >= $height-1) {
+				if ($sx < 0 || $sy < 0 || $sx >= $width - 1 || $sy >= $height - 1) {
 					continue;
 				} else {
 					$color = imagecolorat($img, $sx, $sy) & 0xFF;
-					$color_x = imagecolorat($img, $sx+1, $sy) & 0xFF;
-					$color_y = imagecolorat($img, $sx, $sy+1) & 0xFF;
-					$color_xy = imagecolorat($img, $sx+1, $sy+1) & 0xFF;
+					$color_x = imagecolorat($img, $sx + 1, $sy) & 0xFF;
+					$color_y = imagecolorat($img, $sx, $sy + 1) & 0xFF;
+					$color_xy = imagecolorat($img, $sx + 1, $sy + 1) & 0xFF;
 				}
 
 				if ($color == 255 && $color_x == 255 && $color_y == 255 && $color_xy == 255) {
@@ -197,24 +195,24 @@ class KCaptcha
 					$newgreen = $foreground_color[1];
 					$newblue = $foreground_color[2];
 				} else {
-					$frsx = $sx-floor($sx);
-					$frsy = $sy-floor($sy);
-					$frsx1 = 1-$frsx;
-					$frsy1 = 1-$frsy;
+					$frsx = $sx - floor($sx);
+					$frsy = $sy - floor($sy);
+					$frsx1 = 1 - $frsx;
+					$frsy1 = 1 - $frsy;
 
 					$newcolor = (
-						$color*$frsx1*$frsy1+
-						$color_x*$frsx*$frsy1+
-						$color_y*$frsx1*$frsy+
-						$color_xy*$frsx*$frsy);
+						$color * $frsx1 * $frsy1 +
+						$color_x * $frsx * $frsy1 +
+						$color_y * $frsx1 * $frsy +
+						$color_xy * $frsx * $frsy);
 
-					if ($newcolor>255) $newcolor=255;
-					$newcolor=$newcolor/255;
-					$newcolor0=1-$newcolor;
+					if ($newcolor > 255) $newcolor = 255;
+					$newcolor = $newcolor / 255;
+					$newcolor0 = 1 - $newcolor;
 
-					$newred=$newcolor0*$foreground_color[0]+$newcolor*$background_color[0];
-					$newgreen=$newcolor0*$foreground_color[1]+$newcolor*$background_color[1];
-					$newblue=$newcolor0*$foreground_color[2]+$newcolor*$background_color[2];
+					$newred = $newcolor0 * $foreground_color[0] + $newcolor * $background_color[0];
+					$newgreen = $newcolor0 * $foreground_color[1] + $newcolor * $background_color[1];
+					$newblue = $newcolor0 * $foreground_color[2] + $newcolor * $background_color[2];
 				}
 
 				imagesetpixel($img2, $x, $y, imagecolorallocate($img2, $newred, $newgreen, $newblue));
