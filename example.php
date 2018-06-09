aedee8be3dde0665171747e6d125a6deb641ca9f
Evgeniy Moiseenko <s1lverhron@gmail.com>
2018-06-09 14:26:08 +0300

    First commit

---
+++ example.php
@@ -0,0 +1,10 @@
+<?php
+include('kcaptcha.php');
+
+session_start();
+
+$captcha = new KCAPTCHA();
+
+if ($_REQUEST[session_name()]) {
+	$_SESSION['captcha_keystring'] = $captcha->getKeyString();
+}
