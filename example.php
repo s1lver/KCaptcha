943eb3c7af2f4de63ade7103cbfccc8b8b274a3d
Evgeniy Moiseenko <s1lverhron@gmail.com>
2018-06-11 16:12:23 +0300

    Update example

--- example.php
+++ example.php
@@ -6,7 +6,7 @@ session_start();
 $captcha = new \k_captcha\KCaptcha();
 
 if ($_REQUEST[session_name()]) {
-	$_SESSION['captcha'] = $captcha->getKeyString();
+	$_SESSION['captcha'] = $captcha->keystring;
 }
 ?>
 <img src="example.php?<?= session_name(); ?>=<?= session_id(); ?>">
